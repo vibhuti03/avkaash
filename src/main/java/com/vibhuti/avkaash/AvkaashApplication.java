7e6597259a5d8c691030f3ea609e897c6f877f1f
vmahesh8 <maheshwari_vibhuti@optum.com>
2023-05-18 16:53:37 +0530

    Setting up the platform

---
+++ src/main/java/com/vibhuti/avkaash/AvkaashApplication.java
@@ -0,0 +1,13 @@
+package com.vibhuti.avkaash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvkaashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvkaashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vibhuti/avkaash/AvkaashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibhuti.avkaash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvkaashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
